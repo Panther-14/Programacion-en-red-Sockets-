3968389f44f46d9df58cc8e31d2831a3bbdfa97a
Juan Pablo Peredo <72714359+Panther-14@users.noreply.github.com>
2022-10-14 19:09:22 -0500

    Add files via upload

---
+++ TcpBiding/TcpBiding.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using TcpBiding.Contracts;
+
+namespace TcpBiding.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to enter");
+            Console.ReadLine();
+            var uri = "net.tcp://localhost:4345/ProductService";
+            NetTcpBinding binding = new NetTcpBinding(SecurityMode.None);
+            var channel = new ChannelFactory<IProductService>(binding);
+            var endpoint = new EndpointAddress(uri);
+            var proxy = channel.CreateChannel(endpoint);
+            proxy?.GetStrings().ToList().ForEach(p => Console.WriteLine(p));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TcpBiding/TcpBiding.Server/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using TcpBiding.Contracts;
+
+namespace TcpBiding.Server
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ProductService : IProductService
+    {
+        string[] IProductService.GetStrings()
+        {
+            return new[] { "server1","server2","server3" };
+        }
+    }
+}
